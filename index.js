1ffbd794689e55585463995dbdb1916cc65f0c0f
Benjamin Doherty <benjamin.doherty@snapchat.com>
2018-01-29 22:20:31 -0800

    Bug fixing with node-postrgres

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-const pg = require('pg')
+const { Pool, Client } = require('pg')
+const connectionString = process.env.DATABASE_URL
 
-var pool = new pg.Pool()
+const pool = new Pool({
+  connectionString: connectionString,
+})
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -15,16 +18,15 @@ express()
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({'memebot': true}));
   })
-  .get('/db', (req, res) => {
-    pool.connect(process.env.DATABASE_URL, function(err, client, done) {
-      client.query('SELECT * FROM test_table', function(err, result) {
-        done();
-        if (err) {
-          console.error(err); res.send("Error " + err);
-        } else {
-          res.render('pages/db', {results: results.rows} );
-        }
-      });
-    });
+  .get('/db', (req, response) => {
+    pool.query('SELECT NOW()', (err, res) => {
+      console.log(err, res)
+      if (err) {
+        response.send(err)
+      } else {
+        response.send(res)
+      }
+      pool.end()
+    })
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
