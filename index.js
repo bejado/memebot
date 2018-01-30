22a188a3af945fb755e2904bc846b9b3df1f6d0e
Benjamin Doherty <benjamin.doherty@snapchat.com>
2018-01-29 22:03:00 -0800

    Add a test database

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const pg = require('pg')
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -10,5 +12,17 @@ express()
   .get('/endpoint', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({'memebot': true}));
+  })
+  .get('/db', function (req, res) => {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query('SELECT * FROM test_table', function(err, result) {
+        done();
+        if (err) {
+          console.error(err); res.send("Error " + err);
+        } else {
+          res.render('pages/db', {results: results.rows} );
+        }
+      });
+    });
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
