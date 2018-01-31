ba3848cf33b3d73e8721a1dcff230f6fb39840b3
Benjamin Doherty <benjamin.doherty@snapchat.com>
2018-01-30 20:02:14 -0800

    Implement proof of concept for queueing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -10,14 +11,31 @@ const pool = new Pool({
   ssl: true,  // necessary for local development
 })
 
+const url = process.env.CLOUDAMQP_URL || 'amqp://localhost';
+const open = require('amqplib').connect(url);
+const queue = 'tasks';
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/endpoint', (req, res) => {
+  .post('/endpoint', (req, res) => {
+    if (!req.body.message) {
+      res.status(500);
+      res.send('Something broke!');
+    }
+    open.then(function(conn) {
+      var ok = conn.createChannel();
+      ok = ok.then(function(ch) {
+        ch.assertQueue(queue);
+        ch.sendToQueue(queue, new Buffer(req.body.message));
+      });
+      return ok;
+    }).then(null, console.warn);
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({'memebot': true}));
+    res.send(JSON.stringify({'success': true}));
   })
   .get('/db', (req, res) => {
     pool.query('SELECT * FROM test_table', (err, result) => {
--- worker.js
+++ worker.js
@@ -1,1 +1,17 @@
-console.log('This is a worker process')
+const url = process.env.CLOUDAMQP_URL || 'amqp://localhost';
+const open = require('amqplib').connect(url);
+const queue = 'tasks';
+
+open.then(function(conn) {
+  var ok = conn.createChannel();
+  ok = ok.then(function(ch) {
+    ch.assertQueue(queue);
+    ch.consume(queue, function(msg) {
+      if (msg !== null) {
+        console.log(msg.content.toString());
+        ch.ack(msg);
+      }
+    });
+  });
+});
+
