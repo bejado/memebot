a0dbf8db695f401ab6543573fe737c03967deab5
Benjamin Doherty <benjdoherty15@gmail.com>
2018-02-04 12:32:05 -0800

    Add redux and routing capabilities

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store, { history } from './store'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const target = document.getElementById('root')
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+    <div>
+      <App />
+    </div>
+    </ConnectedRouter>
+  </Provider>,
+  target
+)
+
 registerServiceWorker();
