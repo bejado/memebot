d952c72e3bfe49887009527eb926df3f29466dea
Benjamin Doherty <benjdoherty15@gmail.com>
2018-02-03 20:40:52 -0800

    Generate a concatenation video

--- video_scripts/test_script.sh
+++ video_scripts/test_script.sh
@@ -2,6 +2,10 @@
 # $1- result name
 # $2- youtube video link
 
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 function cleanup {
     echo "Cleaning up..."
     exit 1
@@ -27,7 +31,20 @@ logInfo "Checking that youtube-dl is installed"
 youtube-dl --version >/dev/null 2>&1 || errorAndCleanup "youtube-dl not installed"
 
 # Do our work in the generated directory
-pushd generated
-youtube-dl -f mp4 -o "$1.mp4" "$2"
+if [[ ! -d generated ]]; then
+    mkdir generated
+fi
+pushd generated/
+
+# Download the YouTube video
+youtubeOutput="${1}-source.mp4"
+youtube-dl -f mp4 -o "$youtubeOutput" "$2"
+
+# Combine the videos
+${DIR}/combine.sh \
+    "${DIR}/../video_sources/one_thing_to_say.mp4" 0 7.4 \
+    "${youtubeOutput}" 38 3.5 \
+    "${1}.mp4"
+
 popd
 
