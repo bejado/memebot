3f2d822486f9c9d05d7fe393b9b8947724188141
Benjamin Doherty <benjdoherty15@gmail.com>
2018-02-04 22:54:36 -0800

    Add redux-thunk

--- react-ui/src/store.js
+++ react-ui/src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { routerMiddleware } from 'react-router-redux'
-// import thunk from 'redux-thunk'
+import thunk from 'redux-thunk'
 import createHistory from 'history/createBrowserHistory'
 import rootReducer from './modules'
 
@@ -9,7 +9,7 @@ export const history = createHistory()
 const initialState = {}
 const enhancers = []
 const middleware = [
-  // thunk,
+  thunk,
   routerMiddleware(history)
 ]
 
