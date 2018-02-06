deacb37d6a099c0de20a120a1dd7e26b7adf6d8e
Benjamin Doherty <benjdoherty15@gmail.com>
2018-02-05 20:03:00 -0800

    Port job enqueue API to redux

--- react-ui/src/Home.js
+++ react-ui/src/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { updateJobInput, submitJob } from './modules/jobs'
+import { updateJobInput, enqueueJob } from './modules/jobs'
 
 const Home = props => (
   <div className="App">
@@ -12,17 +12,17 @@ const Home = props => (
       Input a YouTube video URL:
     </p>
     <input type="text" value={props.inputValue} onChange={(e) => props.handleInputChange(e.target.value)} />
-    <button onClick={props.submitJob}>Go!</button>
+    <button onClick={() => props.enqueueJob(props.inputValue)}>Go!</button>
   </div>
 )
 
-const mapStateToProps = state => {
+const mapStateToProps = state => ({
   inputValue: state.jobs.jobInput
-}
+})
 
 const mapDispatchToProps = dispatch => bindActionCreators({
   handleInputChange: (input) => updateJobInput(input),
-  submitJob: () => submitJob()
+  enqueueJob: (message) => enqueueJob(message)
 }, dispatch)
 
 export default connect(
--- react-ui/src/modules/jobs.js
+++ react-ui/src/modules/jobs.js
@@ -1,5 +1,9 @@
 export const UPDATE_JOB_INPUT = 'jobs/UPDATE_JOB_INPUT'
-export const SUBMIT_JOB = 'jobs/SUBMIT_JOB'
+export const ENQUEUE_JOB = 'jobs/ENQUEUE_JOB'
+
+const POST_JOB = 'jobs/POST_JOB'
+const POST_JOB_SUCCESS = 'jobs/POST_JOB_SUCCESS'
+const POST_JOB_FAILURE = 'jobs/POST_JOB_FAILURE'
 
 const initialState = {
   jobInput: '',
@@ -15,11 +19,49 @@ export const updateJobInput = (input) => {
   }
 }
 
-export const submitJob = () => {
+const postJob = (message) => {
+  return {
+    type: POST_JOB,
+    message: message
+  }
+}
+
+const postJobSuccess = (response) => {
+  return {
+    type: POST_JOB_SUCCESS,
+    response: response
+  }
+}
+
+const postJobFailure = (reason) => {
+  return {
+    type: POST_JOB_FAILURE,
+    reason: reason
+  }
+}
+
+export const enqueueJob = (message = '') => {
   return dispatch => {
-    dispatch({
-      type: SUBMIT_JOB
-    })
+    dispatch(postJob(message))
+      const opts = { message };
+      return fetch('/api/job', {
+        method: 'post',
+        body: JSON.stringify(opts),
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        })
+      }).then((response) => {
+        if (response.ok) {
+          return response.json().then((jsonResponse) => {
+            dispatch(postJobSuccess(jsonResponse))
+          })
+        } else {
+          // Assumes that a bad response is in text format
+          return response.text().then((reason) => {
+            dispatch(postJobFailure(reason))
+          })
+        }
+      });
   }
 }
 
@@ -31,10 +73,26 @@ export default (state = initialState, action) => {
         jobInput: action.input
       }
 
-    case SUBMIT_JOB:
+    case POST_JOB:
       return {
         ...state,
         submissionInProgress: true
+      }
+
+    case POST_JOB_SUCCESS:
+      return {
+        ...state,
+        submissionInProgress: false,
+        submissionSuccess: true,
+        submissionResults: action.response
+      }
+
+    case POST_JOB_FAILURE:
+      return {
+        ...state,
+        submissionInProgress: false,
+        submissionSuccess: false,
+        submissionError: action.reason
       }
 
     default:
