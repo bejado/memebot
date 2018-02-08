bd4e4b42c3eab3cea91e98edf6359ddfcb45a2a4
Benjamin Doherty <benjdoherty15@gmail.com>
2018-02-07 23:56:11 -0800

    Refactor worker code into separate function

---
+++ server/job_processing.js
@@ -0,0 +1,26 @@
+const { generateVideo } = require('./video');
+const { updateJob } = require('./database');
+const crypto = require('crypto');
+const { uploadToS3 } = require('./upload');
+
+const processJob = message => {
+  const hash = crypto
+    .createHash('md5')
+    .update(message)
+    .digest('hex');
+  generateVideo(message, hash).then(localPath => {
+    uploadToS3(hash + '.mp4', localPath).then(
+      function(url) {
+        updateJob(hash, 'complete', url).then(() => {
+          console.log(`Job ${hash} status set to complete`);
+        });
+      },
+      function(err) {
+        console.error(err);
+        console.error('Error uploading to S3');
+      }
+    );
+  });
+};
+
+module.exports = { processJob };
--- server/worker.js
+++ server/worker.js
@@ -1,11 +1,8 @@
 const url = process.env.CLOUDAMQP_URL || 'amqp://localhost';
 const open = require('amqplib').connect(url);
-const queue = 'tasks';
-const { generateVideo } = require('./video');
-const { updateJob } = require('./database');
-const crypto = require('crypto');
+const { processJob } = require('./job_processing');
 
-const { uploadToS3 } = require('./upload');
+const queue = 'tasks';
 
 open.then(function(conn) {
   var ok = conn.createChannel();
@@ -15,23 +12,7 @@ open.then(function(conn) {
       if (msg !== null) {
         const message = msg.content.toString();
         console.log('Processing message: ' + message);
-        const hash = crypto
-          .createHash('md5')
-          .update(message)
-          .digest('hex');
-        generateVideo(message, hash).then(localPath => {
-          uploadToS3(hash + '.mp4', localPath).then(
-            function(url) {
-              updateJob(hash, 'complete', url).then(() => {
-                console.log(`Job ${hash} status set to complete`);
-              });
-            },
-            function(err) {
-              console.error(err);
-              console.error('Error uploading to S3');
-            }
-          );
-        });
+        processJob(message);
         ch.ack(msg);
       }
     });
